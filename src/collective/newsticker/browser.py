f1d6e5aea7ad8949fc9fa72cc7fd21290303d0a7
Joaquín Rosales <globojorro@gmail.com>
2011-10-10 11:24:26 -0500

    Cleaned up viewlet code

--- src/collective/newsticker/browser.py
+++ src/collective/newsticker/browser.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
+import json
 
 from five import grok
+from zope.component import queryUtility
 from zope.interface import Interface
 
 from plone.app.layout.viewlets.interfaces import IAboveContent
 from plone.app.layout.viewlets.interfaces import IHtmlHeadLinks
+from plone.registry.interfaces import IRegistry
 
+from collective.newsticker.controlpanel import INewsTickerSettings
 from collective.newsticker.interfaces import INewsTickerLayer
 
 
@@ -16,10 +20,9 @@ class NewsTicker_Viewlet(grok.Viewlet):
     grok.layer(INewsTickerLayer)
     grok.name('collective.newsticker.viewlet')
     grok.order(0)
+    grok.require('zope2.View')
     grok.viewletmanager(IAboveContent)
 
-    def update(self):
-        pass
 
 class CSSLink_Viewlet(grok.Viewlet):
     grok.context(Interface)
