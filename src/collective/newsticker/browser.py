42ccd2bf5b520a170afeb6d7adf0afbb1b89d85c
Joaquín Rosales <globojorro@gmail.com>
2011-10-13 08:55:05 -0500

    Added code to avoid displaying the ticker when no collection or values are available

--- src/collective/newsticker/browser.py
+++ src/collective/newsticker/browser.py
@@ -55,8 +55,15 @@ class NewsTicker_API(grok.View):
     def getItems(self):
         path = self.settings.html_source
         if path:
-            collection = self.context.unrestrictedTraverse(path)
-            return collection.queryCatalog()
+            try:
+                collection = self.context.unrestrictedTraverse(path)
+                return collection.queryCatalog()
+            except:
+                return []
+
+    def hasItems(self):
+        items = self.getItems()
+        return len(items) > 0
 
     def dumps(self, json_var=None, sort_keys=True, indent=0):
         """ """
