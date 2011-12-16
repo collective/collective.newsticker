380444a56ef3f74d6114d21168bd4c38d805b2f9
hvelarde <hector.velarde@gmail.com>
2011-12-15 20:57:47 -0600

    added browser layer installation tests

--- src/collective/newsticker/tests/test_setup.py
+++ src/collective/newsticker/tests/test_setup.py
@@ -5,6 +5,8 @@ import unittest2 as unittest
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
 
+from plone.browserlayer.utils import registered_layers
+
 from collective.newsticker.config import PROJECTNAME
 from collective.newsticker.testing import INTEGRATION_TESTING
 
@@ -21,6 +23,11 @@ class InstallTest(unittest.TestCase):
     def test_installed(self):
         qi = getattr(self.portal, 'portal_quickinstaller')
         self.failUnless(qi.isProductInstalled(PROJECTNAME))
+
+    def test_browserlayer_installed(self):
+        layers = [l.getName() for l in registered_layers()]
+        self.assertTrue('INewsTickerLayer' in layers,
+                        'browser layer not installed')
 
     def test_javascript_installed(self):
         js = getattr(self.portal, 'portal_javascripts')
@@ -39,6 +46,11 @@ class UninstallTest(unittest.TestCase):
 
     def test_uninstalled(self):
         self.failIf(self.qi.isProductInstalled(PROJECTNAME))
+
+    def test_browserlayer_installed(self):
+        layers = [l.getName() for l in registered_layers()]
+        self.assertTrue('INewsTickerLayer' not in layers,
+                        'browser layer not removed')
 
     def test_javascript_installed(self):
         js = getattr(self.portal, 'portal_javascripts')
